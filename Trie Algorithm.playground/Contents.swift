765629fa3656fab01ce7d7c3c27b77767b2ca2a6
Ashish Pisey <ashishpisey21@gmail.com>
2021-10-27 12:01:13 +0530

    updated trie demo

--- Trie Algorithm.playground/Contents.swift
+++ Trie Algorithm.playground/Contents.swift
@@ -40,18 +40,21 @@ let cNode = Node()
 cNode.children = ["c": a]
 cNode.isWord = false
 
+var wordArr:[[Node]] = []
+var nodeArr:[Node] = []
 
-func searchTrie(node:Node?) -> Node? {
+// single search
+func singleWordSearch(node:Node?) -> Node? {
     // define nodes
     guard let node = node else { return nil }
-    print(node.children.keys.first)
+    print(node.children.keys.first!)
     
     // check the base condition
     if node.children.count > 0 {
         if node.isWord == false {
             let key = node.children.keys.first
             //previousArr.append(node)
-            guard let nextNode = node.children[key!] as? Node else {return node}// O(1)
+            guard let nextNode = node.children[key!] else {return node}// O(1)
             searchTrie(node: nextNode)
         }
         return node
@@ -60,7 +63,84 @@ func searchTrie(node:Node?) -> Node? {
     return nil
 }
 
+//MARK: - solution 1 - Multiple word search
+func searchTrie(node:Node?) -> Node? {
+    // define nodes
+    guard let node = node else { return nil }
+    nodeArr.append(node)
+    
+    // check the base condition
+    if node.children.count > 0  {
+        
+        if node.isWord == false {
+            guard let nextNodeArr = getNextNode(node: node) else { return nil }
+            for nextNode in nextNodeArr {
+                searchTrie(node: nextNode)
+            }
+        } else {
+            //print("last letter: \(nodeArr.last?.children.keys.first)")
+            if wordArr.count > 0 {
+                //reuse word arr
+                var commonArr = wordArr[0]
+                commonArr.removeLast()
+                commonArr.append(nodeArr.last!)
+                wordArr.append(commonArr)
+            } else {
+                wordArr.append(nodeArr)
+            }
+            nodeArr = []
+        }
+        
+        return node
+    }
+    
+    return nil
+}
+
+func printCombinationOfWords(wordArr:[[Node]]) {
+    for word in wordArr {
+        for node in word {
+            print(node.children.keys.first!)
+        }
+        print("word")
+    }
+}
+
+func getNextNode(node:Node) -> [Node]? {
+    let keys = node.children.keys
+
+    var nextNodeArr:[Node] = []
+    for key in keys {
+        guard let nextNode = node.children[key] else { return nil }// O(1)
+        nextNodeArr.append(nextNode)
+    }
+    return nextNodeArr
+}
+
+//MARK: - solution 2
+func getNextLetters(node:Node) -> [Character]? {
+    let keys = node.children.keys
+
+    var nextNodeArr:[Node] = []
+    for key in keys {
+        guard let nextNode = node.children[key] else { return nil }// O(1)
+        nextNodeArr.append(nextNode)
+    }
+    
+    var nextLetters:[Character] = []
+    for nextNode in nextNodeArr {
+        guard let nextLetter = nextNode.children.keys.first else { return nil }
+        nextLetters.append(nextLetter)
+    }
+    return nextLetters
+}
+
+wordArr = []
 searchTrie(node: cNode)
+//print(wordArr)
+printCombinationOfWords(wordArr: wordArr)
 
-
-
+// C : Node
+// A
+// R
+// D // T
