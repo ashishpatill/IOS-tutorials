93f04c44b6f1abe5e72f4e10cc1512b43217750a
Ashish Pisey <ashishpisey21@gmail.com>
2021-10-27 17:40:22 +0530

    Swift UI Demo

---
+++ SwiftUIdemo/SwiftUIdemo/SwiftUIdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIdemoApp.swift
+//  SwiftUIdemo
+//
+//  Created by Ashish Pisey on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
